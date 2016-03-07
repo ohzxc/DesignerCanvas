2755dd9f91d2950a4b4bc1484107d32d0ac9c8d3
ohzxc <zhouxc@qq.com>
2016-03-07 15:18:10 +0800

    属性窗口增加提示

--- DesignerCanvas/Controls/EditSubTrade.xaml.cs
+++ DesignerCanvas/Controls/EditSubTrade.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 //using Public.Common;
 //using Public.Communication;
 //using Platform.Common;
@@ -21,7 +12,7 @@ namespace DesignerCanvas.Controls
     {
         public string TradeCode { get; set; }
         //public delegate void PassValuesHandler(List<SubRel> e);
-
+        public List<SubRel> SubReList { get; set; }
         //public event PassValuesHandler PassValuesEvent; 
         public EditSubTrade(string tradecode,string name, string item, string flowcode, string tradeFlow, List<SubRel> subReList, List<string> conditionList)
         {
@@ -31,8 +22,22 @@ namespace DesignerCanvas.Controls
             tbId.Text = item;
             tbFlowCode.Text = flowcode;
             lvConditionList.ItemsSource = conditionList;
-            
+
             dgRel.DataContext = subReList;
+            SubReList = new List<SubRel>();
+            subReList.ForEach(x =>
+            {
+                SubReList.Add(new SubRel()
+                {
+                    //CompCode =(string) x.CompCode.Clone(),
+                    InData = (string)x.InData.Clone(),
+                    InType = x.InType,
+                    Memo = (string)x.Memo.Clone(),
+                    OutData = (string)x.OutData.Clone(),
+                    //SerialNumber = (string)x.SerialNumber.Clone(),
+                    //TradeCode = (string)x.TradeCode.Clone()
+                });
+            });
             tbTradeFLow.Text = tradeFlow;
         }
         
@@ -47,6 +52,7 @@ namespace DesignerCanvas.Controls
         }
         #endregion
 
+        private bool _isOK=false;
         private void btnOK_Click(object sender, RoutedEventArgs e)
         {
             //PassValuesEvent(dgRel.DataContext as List<SubRel>);
@@ -54,31 +60,61 @@ namespace DesignerCanvas.Controls
             //if (Validation.GetHasError(dgRel)) return;
             //dgRel.CommitEdit();
             //dgRel.CommitEdit(DataGridEditingUnit.Row, true);
-
-            var hasError = false;
-            for (int i = 0; i < dgRel.Items.Count; i++)
-            {
-                DependencyObject o = dgRel.ItemContainerGenerator.ContainerFromIndex(i);
-                hasError = Validation.GetHasError(o);
-                if (hasError)
-                {
-                    return;
-                }
-            }
+            _isOK = true;
             this.Close();
         }
         protected override void OnClosing(CancelEventArgs e)
         {
             btnOK.Focus();
             var hasError = false;
-            for (int i = 0; i < dgRel.Items.Count; i++)
+            if(_isOK)
             {
-                DependencyObject o = dgRel.ItemContainerGenerator.ContainerFromIndex(i);
-                hasError = Validation.GetHasError(o);
-                if (hasError)
+                for (int i = 0; i < dgRel.Items.Count; i++)
                 {
-                    e.Cancel = true;
-                    return;
+                    DependencyObject o = dgRel.ItemContainerGenerator.ContainerFromIndex(i);
+                    hasError = Validation.GetHasError(o);
+                    if (hasError)
+                    {
+                        MessageBox.Show("参数填写不正确，请将鼠标悬停到红色框内查看具体信息。", "提示");
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("是否保存？", "提示", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+                    for (int i = 0; i < dgRel.Items.Count; i++)
+                    {
+                        DependencyObject o = dgRel.ItemContainerGenerator.ContainerFromIndex(i);
+                        hasError = Validation.GetHasError(o);
+                        if (hasError)
+                        {
+                            MessageBox.Show("参数填写不正确，请将鼠标悬停到红色框内查看具体信息。", "提示");
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    //TODO 异常
+                    (dgRel.DataContext as List<SubRel>).Clear();
+                    SubReList.ForEach(x =>
+                    {
+                        (dgRel.DataContext as List<SubRel>).Add(new SubRel()
+                        {
+                            //CompCode =(string) x.CompCode.Clone(),
+                            InData = (string)x.InData,
+                            InType = x.InType,
+                            Memo = (string)x.Memo,
+                            OutData = (string)x.OutData,
+                            //SerialNumber = (string)x.SerialNumber.Clone(),
+                            //TradeCode = (string)x.TradeCode.Clone()
+                        });
+                    });
+                    dgRel.Items.Refresh();
                 }
             }
             (dgRel.DataContext as List<SubRel>).ForEach(x =>
@@ -87,6 +123,7 @@ namespace DesignerCanvas.Controls
                 x.SerialNumber = tbFlowCode.Text;
                 x.TradeCode = TradeCode;
             });
+
             //Do whatever you want here..
         }
        
--- DesignerCanvas/DesignerItem.cs
+++ DesignerCanvas/DesignerItem.cs
@@ -259,6 +259,7 @@ namespace DesignerCanvas
 
                 //editSubTrade.PassValuesEvent += new EditSubTrade.PassValuesHandler(ReceiveValues);
                 editSubTrade.ShowDialog();
+                
             }
             catch (Exception e)
             {
