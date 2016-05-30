b6d0c4d98b6eadc2ef8eaa14f07af4b88a59576b
ohzxc <zhouxc@qq.com>
2016-05-30 11:51:21 +0800

    添加一下基本属性，类

---
+++ FlowChart/FlowItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using FlowChart.Interfaces;
+
+namespace FlowChart
+{
+    public class FlowItem : ContentControl, ISelectable, IGroupable, INotifyPropertyChanged
+    {
+        public string CurrentSerialNumber
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        ///  
+        /// </summary>
+        public bool IsGroup { get; set; }
+
+        /// <summary>
+        /// 是否被选中
+        /// </summary>
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+        public static readonly DependencyProperty IsSelectedProperty =
+          DependencyProperty.Register("IsSelected",
+                                       typeof(bool),
+                                       typeof(FlowItem),
+                                       new FrameworkPropertyMetadata(false));
+
+        public string ParentSerialNumber
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #region 实现INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
---
+++ FlowChart/Interfaces/ISelectable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowChart.Interfaces
+{
+    /// <summary>
+    /// 定义选中元素的接口
+    /// </summary>
+    public interface ISelectable
+    {
+        /// <summary>
+        /// 是否选中
+        /// </summary>
+        bool IsSelected { get; set; }
+    }
+}
